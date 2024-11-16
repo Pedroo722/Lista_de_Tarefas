bd2c8cf07eb6e1d19a50e95e8cf396aeb2d51245
Pedroo722 <pedrohenriquealexandre7@gmail.com>
2024-11-16 19:35:49 -0300

    chore: constants.ts

---
+++ src/util/constants.ts
@@ -0,0 +1,5 @@
+export const ERRO_SERVIDOR = 'Erro na comunicação com o servidor!';
+
+export const baseUrl = 'https://lista-de-tarefas-api-6r8k.onrender.com/api/';
+
+export const baseUrlTasks = baseUrl + 'tasks';
