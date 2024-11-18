20aa06e016c23f96bc1c8682a6af0dc80b90f8bf
Pedroo722 <pedrohenriquealexandre7@gmail.com>
2024-11-18 14:22:30 -0300

    feature: Adição, Ediçaõ e Remoção de Tarefas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, IconButton, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
+import { Button, Box, Paper,
+  Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
+  IconButton, CircularProgress,
+  Typography, TextField,
+  Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
 import { Edit, Delete, ArrowUpward, ArrowDownward } from '@mui/icons-material';
+import { Modal } from 'antd';
 import { baseUrlTasks } from '../util/constants';
 import axios from 'axios';
 import { Task } from '../util/types';
+import PageSelector from '../components/PageSelector';
+import SearchBar from '../components/SearchBar';
 
 const Home: React.FC = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [filteredTasks, setFilteredTasks] = useState<Task[]>([]);
   const [openDialog, setOpenDialog] = useState<boolean>(false);
   const [currentTask, setCurrentTask] = useState<Task | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [loadingMessage, setLoadingMessage] = useState<string>('');
+  const [openConfirmDialog, setOpenConfirmDialog] = useState(false);
+  const [taskToDelete, setTaskToDelete] = useState<Task | null>(null);
+  const [openErrorDialog, setOpenErrorDialog] = useState(false);
+  const [openSuccessDialog, setOpenSuccessDialog] = useState(false);
+  const [dialogMessage, setDialogMessage] = useState<string>('');
+  const [dialogTitle, setDialogTitle] = useState<string>('');
+  const tasksPerPage = 7;
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [searchQuery, setSearchQuery] = useState<string>('');
 
+  // Carregar Tarefas
   const fetchTasks = () => {
+    setLoading(true);
+    setLoadingMessage('Carregando tarefas...');
     axios
       .get(baseUrlTasks)
       .then((response) => {
         if (response.status === 200) {
           const dados = response.data;
-          console.log('Dados Recebidos: ', dados);
           const sortedTasks = dados
             .map((task: Task) => ({
               id: task.id,
@@ -28,22 +49,60 @@ const Home: React.FC = () => {
             .sort((a: Task, b: Task) => a.ordemApresentacao - b.ordemApresentacao);
 
           setTasks(sortedTasks);
-          console.log('Tasks fetched and sorted successfully!');
+          setFilteredTasks(sortedTasks);
         } else {
-          alert('Erro ao buscar tarefas no servidor!');
+          setDialogMessage('Erro ao buscar tarefas no servidor!');
+          setDialogTitle('Erro');
+          setOpenErrorDialog(true);
         }
       })
       .catch((error) => {
         console.error(error);
-        alert('Erro ao conectar com o servidor!');
+        setDialogMessage('Erro ao conectar com o servidor!');
+        setDialogTitle('Erro');
+        setOpenErrorDialog(true);
+      })
+      .finally(() => {
+        setLoading(false);
+        setLoadingMessage('');
       });
   };
 
+  // Pesquisa
+  const handleSearch = (query: string) => {
+    setSearchQuery(query);
+    if (query === '') {
+      setFilteredTasks(tasks);
+    } else {
+      setFilteredTasks(tasks.filter((task) => task.nomeTarefa.toLowerCase().includes(query.toLowerCase())));
+    }
+  };
+
+  const paginateTasks = (tasks: Task[]) => {
+    const startIndex = (currentPage - 1) * tasksPerPage;
+    const endIndex = startIndex + tasksPerPage;
+    return tasks.slice(startIndex, endIndex);
+  };
+
+  // Criar Tarefa
   const handleCreate = (task: Task) => {
+    setLoading(true);
+    setLoadingMessage('Criando tarefa...');
+  
+    // Verificação para impedir Tarefas com mesmo nome
+    const taskExists = tasks.some((existingTask) => existingTask.nomeTarefa === task.nomeTarefa);
+    if (taskExists) {
+      setDialogMessage('Já existe uma tarefa com esse nome!');
+      setDialogTitle('Erro');
+      setOpenErrorDialog(true);
+      setLoading(false);
+      return;
+    }
+  
     const taskToCreate = {
       nomeTarefa: task.nomeTarefa,
       custo: task.custo,
-      dataLimite: task.dataLimite || ''
+      dataLimite: task.dataLimite || '',
     };
   
     axios
@@ -51,63 +110,110 @@ const Home: React.FC = () => {
       .then((response) => {
         if (response.status === 201) {
           setTasks([...tasks, response.data]);
-          console.log('Task created successfully!');
+          setDialogMessage('Tarefa criada com sucesso!');
+          setDialogTitle('Sucesso');
+          setOpenSuccessDialog(true);
           handleCloseDialog();
         } else {
-          alert('Erro ao criar tarefa no servidor!');
+          setDialogMessage('Erro ao criar tarefa no servidor!');
+          setDialogTitle('Erro');
+          setOpenErrorDialog(true);
         }
       })
       .catch((error) => {
         console.error(error);
-        console.log("Task enviada: ", task);
-        alert('Erro ao conectar com o servidor!');
+        setDialogMessage('Erro ao conectar com o servidor!');
+        setDialogTitle('Erro');
+        setOpenErrorDialog(true);
+      })
+      .finally(() => {
+        setLoading(false);
+        setLoadingMessage('');
       });
   };
-
+  
+  // Editar Tarefa
   const handleEdit = () => {
     if (currentTask) {
+      setLoading(true);
+      setLoadingMessage('Editando tarefa...');
+  
+      // Verificação para impedir Tarefas com mesmo nome
+      const taskExists = tasks.some(
+        (existingTask) =>
+          existingTask.nomeTarefa === currentTask.nomeTarefa && existingTask.id !== currentTask.id
+      );
+      if (taskExists) {
+        setDialogMessage('Já existe uma tarefa com esse nome!');
+        setDialogTitle('Erro');
+        setOpenErrorDialog(true);
+        setLoading(false);
+        return;
+      }
+  
       axios
         .put(`${baseUrlTasks}/${currentTask.id}`, currentTask)
         .then((response) => {
           if (response.status === 200) {
             setTasks(tasks.map((task) => task.id === currentTask.id ? response.data : task));
-            console.log('Task edited successfully!');
+            setDialogMessage('Tarefa editada com sucesso!');
+            setDialogTitle('Sucesso');
+            setOpenSuccessDialog(true);
           } else {
-            alert('Erro ao editar tarefa no servidor!');
+            setDialogMessage('Erro ao editar tarefa no servidor!');
+            setDialogTitle('Erro');
+            setOpenErrorDialog(true);
           }
         })
         .catch((error) => {
           console.error(error);
-          alert('Erro ao conectar com o servidor!');
+          setDialogMessage('Erro ao conectar com o servidor!');
+          setDialogTitle('Erro');
+          setOpenErrorDialog(true);
         })
         .finally(() => {
+          setLoading(false);
+          setLoadingMessage('');
           handleCloseDialog();
         });
     }
   };
 
-  const handleDelete = (id: number) => {
-    if (window.confirm('Tem certeza que deseja apagar?')) {
+  // Deletar Tarefa
+  const handleDelete = () => {
+    if (taskToDelete) {
+      setLoading(true);
+      setLoadingMessage('Excluindo tarefa...');
       axios
-        .delete(`${baseUrlTasks}/${id}`)
-        .then((response) => {
-          if (response.status === 200) {
-            console.log('Task deleted successfully!');
-            fetchTasks();
-          }
+        .delete(`${baseUrlTasks}/${taskToDelete.id}`)
+        .then(() => {
+          setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskToDelete.id));
+          setDialogMessage('Tarefa excluída com sucesso!');
+          setDialogTitle('Sucesso');
+          setOpenSuccessDialog(true);
         })
         .catch((error) => {
           console.error(error);
-          alert('Erro ao conectar com o servidor!');
+          setDialogMessage('Erro ao conectar com o servidor!');
+          setDialogTitle('Erro');
+          setOpenErrorDialog(true);
+        })
+        .finally(() => {
+          setLoading(false);
+          setLoadingMessage('');
+          setOpenConfirmDialog(false);
         });
     }
   };
 
+  // Reordenação
   const handleReorder = (id: number, direction: 'up' | 'down') => {
-    // const index = tasks.findIndex((task) => task.id === id);
-    alert("Em implementação...");
+    setDialogMessage("Em implementação...");
+    setDialogTitle("Atenção");
+    setOpenErrorDialog(true);
   };
 
+  // Abrir Modal
   const handleOpenDialog = (task: Task) => {
     setCurrentTask(task);
     setOpenDialog(true);
@@ -118,40 +224,73 @@ const Home: React.FC = () => {
     setCurrentTask(null);
   };
 
+  // Confirmação de Deletação
+  const openDeleteConfirmation = (task: Task) => {
+    setTaskToDelete(task);
+    setOpenConfirmDialog(true);
+  };
+
+  const closeDeleteConfirmation = () => {
+    setOpenConfirmDialog(false);
+    setTaskToDelete(null);
+  };
+
   useEffect(() => {
     fetchTasks();
   }, []);
 
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Lista de Fazeres</h1>
-      <TableContainer>
+    <Box sx={{ padding: 3 }}>
+      <Typography variant="h4" component="h1" sx={{ marginBottom: 3 }}>
+        Lista de Fazeres
+      </Typography>
+
+      <SearchBar query={searchQuery} onSearch={handleSearch} />
+
+      {loading && (
+        <Box sx={{ textAlign: 'center', marginBottom: 3 }}>
+          <CircularProgress />
+          <Typography variant="h6" sx={{ marginTop: 2 }}>
+            {loadingMessage}
+          </Typography>
+        </Box>
+      )}
+
+      <TableContainer component={Paper}>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Nome</TableCell>
-              <TableCell>Custo (R$)</TableCell>
-              <TableCell>Data Limite</TableCell>
-              <TableCell>Ações</TableCell>
+              <TableCell><strong>Nome</strong></TableCell>
+              <TableCell><strong>Custo (R$)</strong></TableCell>
+              <TableCell><strong>Data Limite</strong></TableCell>
+              <TableCell><strong>Ações</strong></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {tasks.map((task) => (
-              <TableRow key={task.id}>
+            {paginateTasks(filteredTasks).map((task) => (
+              <TableRow
+                key={task.id}
+                sx={{
+                  '&:hover': {
+                    backgroundColor: '#f1f1f1',
+                  },
+                  backgroundColor: task.custo >= 1000 ? 'yellow' : 'transparent',
+                }}
+              >
                 <TableCell>{task.nomeTarefa}</TableCell>
                 <TableCell>{task.custo}</TableCell>
                 <TableCell>{task.dataLimite}</TableCell>
                 <TableCell>
-                  <IconButton onClick={() => handleOpenDialog(task)}>
+                  <IconButton onClick={() => handleOpenDialog(task)} sx={{ color: 'primary.main' }}>
                     <Edit />
                   </IconButton>
-                  <IconButton onClick={() => handleDelete(task.id)}>
+                  <IconButton onClick={() => openDeleteConfirmation(task)} sx={{ color: 'error.main' }}>
                     <Delete />
                   </IconButton>
-                  <IconButton onClick={() => handleReorder(task.id, 'up')}>
+                  <IconButton onClick={() => handleReorder(task.id, 'up')} sx={{ color: 'primary.main' }}>
                     <ArrowUpward />
                   </IconButton>
-                  <IconButton onClick={() => handleReorder(task.id, 'down')}>
+                  <IconButton onClick={() => handleReorder(task.id, 'down')} sx={{ color: 'primary.main' }}>
                     <ArrowDownward />
                   </IconButton>
                 </TableCell>
@@ -161,11 +300,18 @@ const Home: React.FC = () => {
         </Table>
       </TableContainer>
 
+      <PageSelector
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        tasksPerPage={tasksPerPage}
+        tasks={tasks}
+      />
+
       <Button
         variant="contained"
         color="primary"
         size="large"
-        style={{ marginTop: '20px' }}
+        sx={{ marginTop: 3, backgroundColor: 'green' }}
         onClick={() =>
           handleOpenDialog({
             id: 0,
@@ -179,57 +325,102 @@ const Home: React.FC = () => {
         Inserir Tarefa
       </Button>
 
-      <Dialog open={openDialog} onClose={handleCloseDialog}>
-        <DialogTitle>
-          {currentTask?.id ? 'Editar Tarefa' : 'Criar Tarefa'}
-        </DialogTitle>
+      <Modal
+        title={currentTask?.id ? 'Editar Tarefa' : 'Criar Tarefa'}
+        open={openDialog}
+        onCancel={handleCloseDialog}
+        onOk={() =>
+          currentTask?.id ? handleEdit() : handleCreate(currentTask!)
+        }
+      >
+        <TextField
+          label="Nome da Tarefa"
+          fullWidth
+          margin="normal"
+          value={currentTask?.nomeTarefa || ''}
+          onChange={(e) =>
+            setCurrentTask({ ...currentTask!, nomeTarefa: e.target.value })
+          }
+        />
+        <TextField
+          label="Custo (R$)"
+          fullWidth
+          margin="normal"
+          type="number"
+          value={currentTask?.custo || ''}
+          onChange={(e) =>
+            setCurrentTask({ ...currentTask!, custo: parseFloat(e.target.value) })
+          }
+        />
+        <TextField
+          label="Data Limite"
+          fullWidth
+          margin="normal"
+          type="date"
+          value={currentTask?.dataLimite || ''}
+          onChange={(e) =>
+            setCurrentTask({ ...currentTask!, dataLimite: e.target.value })
+          }
+          slotProps={{inputLabel: { shrink:true } }}
+        />
+      </Modal>
+
+      <Dialog
+        open={openConfirmDialog}
+        onClose={closeDeleteConfirmation}
+      >
+        <DialogTitle>Confirmar Exclusão</DialogTitle>
         <DialogContent>
-          <TextField
-            label="Nome da Tarefa"
-            fullWidth
-            margin="normal"
-            value={currentTask?.nomeTarefa || ''}
-            onChange={(e) =>
-              setCurrentTask({ ...currentTask!, nomeTarefa: e.target.value })
-            }
-          />
-          <TextField
-            label="Custo (R$)"
-            fullWidth
-            margin="normal"
-            type="number"
-            value={currentTask?.custo || ''}
-            onChange={(e) =>
-              setCurrentTask({ ...currentTask!, custo: parseFloat(e.target.value) })
-            }
-          />
-          <TextField
-            label="Data Limite"
-            fullWidth
-            margin="normal"
-            type="date"
-            value={currentTask?.dataLimite || ''}
-            onChange={(e) =>
-              setCurrentTask({ ...currentTask!, dataLimite: e.target.value })
-            }
-            InputLabelProps={{ shrink: true }}
-          />
+          <Typography>Tem certeza que deseja excluir esta tarefa?</Typography>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleCloseDialog} color="primary">
+          <Button
+            onClick={closeDeleteConfirmation}
+            sx={{ color: 'red' }}
+          >
             Cancelar
           </Button>
           <Button
-            onClick={() =>
-              currentTask?.id ? handleEdit() : handleCreate(currentTask!)
-            }
-            color="primary"
+            onClick={handleDelete}
+            sx={{
+              backgroundColor: 'green',
+              color: 'white',
+              '&:hover': {
+                backgroundColor: 'darkgreen',
+              },
+            }}
           >
-            Salvar
+            Confirmar
           </Button>
         </DialogActions>
       </Dialog>
-    </div>
+
+      <Dialog
+        open={openErrorDialog}
+        onClose={() => setOpenErrorDialog(false)}
+      >
+        <DialogTitle>{dialogTitle}</DialogTitle>
+        <DialogContent>
+          <Typography>{dialogMessage}</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpenErrorDialog(false)}>Fechar</Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog
+        open={openSuccessDialog}
+        onClose={() => setOpenSuccessDialog(false)}
+      >
+        <DialogTitle>{dialogTitle}</DialogTitle>
+        <DialogContent>
+          <Typography>{dialogMessage}</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpenSuccessDialog(false)}>Fechar</Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
   );
 };
 
